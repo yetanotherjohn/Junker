5c6573c22ca159b2401cd1ac87922c1e25856d39
john <w.johnathan@gmail.com>
2022-12-22 12:37:15 -0800

    Dragonflight API change
    Misc. cleanup

--- Junker.lua
+++ Junker.lua
@@ -1,41 +1,59 @@
-function Junker()
-	TotalSale = 0
-	for myBags = 0, 4 do
-		for bagSlots = 1, GetContainerNumSlots(myBags) do
-			CurrentItemLink = GetContainerItemLink(myBags, bagSlots)
+local ilvlThreshold = 260
 
-				if CurrentItemLink then
-					_, _, itemQuality, _, _, _, _, _, _, _, itemSellPrice, classID, subclassID, bindType = GetItemInfo(CurrentItemLink)
-                    effectiveILvl, _, _ = GetDetailedItemLevelInfo(CurrentItemLink)  -- avoid selling timewalking loot with effectiveILvl
-					_, itemCount = GetContainerItemInfo(myBags, bagSlots)
+function Junker(debug)
+    local debug = debug or nil;
+    
+    -- WoW throttles sales but the script keeps running
+    -- stop script to avoid useless processing
+    local quantitySold = 0 
 
-					-- debugging
-                    -- print(GetItemInfo(CurrentItemLink))
-                    -- print(GetDetailedItemLevelInfo(CurrentItemLink))
+	for currentBag = 0, 6 do
 
-					-- not debugging
-                    if (itemQuality == 0 and itemSellPrice ~= nil and itemSellPrice ~= 0) or -- is grey and has a vendor price, OR
-                    
-                       (itemQuality < 5 and itemSellPrice ~= nil and itemSellPrice ~= 0) and -- is less than legendary and has a vendor price, AND
-                       (bindType == 1 ) and                         -- is BoP (bound because it's in bags), AND
-                       ((classID == 4) or                           -- is armor, OR
-                       (classID == 2 and subclassID ~= 20)) and     -- is weapon but not fishing poles and, AND
-                       (effectiveILvl <= 80)                        -- is some arbitrarily low item level not even remotely useful as I see it
+        if quantitySold == 12 then
+            break
+        end    
+    
+		for currentSlot = 1, C_Container.GetContainerNumSlots(currentBag) do
+
+            if quantitySold == 12 then
+                break
+            end
+
+            local containerInfo  = C_Container.GetContainerItemInfo(currentBag,currentSlot)
+
+            if containerInfo ~= nil and containerInfo.hyperlink ~= nil then
+                _, _, _, _, _, classID, subclassID = GetItemInfoInstant(containerInfo.hyperlink)
+                effectiveILvl, _, _ = GetDetailedItemLevelInfo(containerInfo.hyperlink)  -- avoid selling timewalking loot with effectiveILvl
+
+                    if
+                    (containerInfo.quality == 0 and containerInfo.hasNoValue == false) or -- is grey and is sellable OR
+                    (containerInfo.quality ~= nil and containerInfo.quality < 5 and containerInfo.hasNoValue == false) and -- is not an oddball item, is less than legendary, is sellable AND
+                    (containerInfo.isBound == true ) and         -- is not transferrable AND
+                    ((classID == 4 and subclassID ~= 5) or       -- is armor but not cosmetic, OR
+                    (classID == 2 and subclassID ~= 20)) and     -- is weapon but not fishing poles, AND
+                    (effectiveILvl <= ilvlThreshold and effectiveILvl > 1) -- is below item level threshold but greater than tabbard ilvl (1)
                     then
-                        TotalSale = TotalSale + (itemSellPrice * itemCount)
-						print(CurrentItemLink .. " x" ..itemCount .. ", " .. GetCoinTextureString(itemSellPrice * itemCount))
-						UseContainerItem(myBags, bagSlots)
-                    end
-				end
-		end
-    end
+                        if debug == true then
+                            print("Mock sale:" .. containerInfo.hyperlink .."," .. containerInfo.quality.."," .. classID.."," .. subclassID .. "," .. effectiveILvl)
+                        else
+                            C_Container.UseContainerItem(currentBag, currentSlot)
+                            quantitySold = quantitySold + 1
+                        end -- if debug
+                    end -- sell cases
+			end -- if currentItemLink
+        end -- for currentSlot
+    end -- for currentBag
+end -- function
 
-	if TotalSale ~= 0 then
-		print("Total Price for all items: " .. GetCoinTextureString(TotalSale))
-	else
-		print("No items were sold.")
+
+-- On Load
+SLASH_JUNKER1 = '/junker';
+SlashCmdList.JUNKER = function(msg,editbox)
+	if msg == 'debug' then
+        Junker(true);
 	end
 end
+
 
 local BtnSellGrey = CreateFrame( "Button" , "SellGreyBtn" , MerchantFrame, "UIPanelButtonTemplate" )
 BtnSellGrey:SetText("Purge")
